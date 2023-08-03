8559d53653517b6a85692845370511548e19a765
Kevin <kevin_jeon@icloud.com>
2023-08-03 09:23:53 -0400

    Initial Commit

---
+++ GolfDistanceTracker/GolfDistanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfDistanceTrackerApp.swift
+//  GolfDistanceTracker
+//
+//  Created by Kevin Mattocks on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GolfDistanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
