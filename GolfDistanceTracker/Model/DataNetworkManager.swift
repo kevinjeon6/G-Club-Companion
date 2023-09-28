b31d7e35b37eb1a4877d07b1c2eecccfdf08de4f
Kevin <kevin_jeon@icloud.com>
2023-09-27 23:00:02 -0400

    Add custom JSON data that contains the golf clubs Create the model for the JSON data

---
+++ GolfDistanceTracker/Model/DataNetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataNetworkManager.swift
+//  GolfDistanceTracker
+//
+//  Created by Kevin Mattocks on 8/29/23.
+//
+
+import Foundation
+
+
+struct ClubResponse: Decodable, Hashable {
+    let name: String
+}
