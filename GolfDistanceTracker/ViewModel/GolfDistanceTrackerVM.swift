186f3e4b631c9df1820e666801279d5265679985
Kevin <kevin_jeon@icloud.com>
2024-01-15 13:34:56 -0500

    Start initially putting in logic into a view model

---
+++ GolfDistanceTracker/ViewModel/GolfDistanceTrackerVM.swift
@@ -0,0 +1,54 @@
+//
+//  GolfDistanceTrackerVM.swift
+//  GolfDistanceTracker
+//
+//  Created by Kevin Mattocks on 12/29/23.
+//
+
+import CoreData
+import Foundation
+
+class GolfDistanceTrackerVM: ObservableObject {
+    
+    @Published var notes = ""
+    @Published var clubBrand = ""
+    @Published var ballBrand = ""
+    @Published var carryDistance = 0
+    @Published var shaftName = ""
+    @Published var flex = "Regular"
+    @Published var loft = "N/A"
+    
+   @Published var shaftFlexType = ["Regular", "Stiff", "X Stiff", "Senior", "Ladies"]
+   @Published var wedgeDegrees = ["N/A", "42°", "44°", "46°", "48°", "50°", "52°", "54°", "56°", "58°", "60°", "62°", "64°"]
+    
+    
+    
+    // MARK: - load JSON Preload data
+    
+    func getPreLoadedJSON(_ file: String, context: NSManagedObjectContext,  firstTime: inout Bool) -> [ClubDetailsEntity] {
+       //Create a URL
+       guard let url = Bundle.main.url(forResource: file, withExtension: "json") else { fatalError("Failed to locate \(file) in bundle")}
+
+       guard let data = try? Data(contentsOf: url) else { fatalError("Failed to load \(file) from bundle")}
+
+       let decoder = JSONDecoder()
+
+       guard let preloadedClubData = try? decoder.decode([ClubResponse].self, from: data) else { fatalError("Failed to decode \(file)")}
+
+
+       var preloadedGolfClubs = [ClubDetailsEntity]()
+
+       if firstTime {
+           for loadedClub in preloadedClubData {
+               let loadedEntity = ClubDetailsEntity(context: context)
+               loadedEntity.name = loadedClub.name
+               preloadedGolfClubs.append(loadedEntity)
+           }
+     
+         
+           firstTime = false
+       }
+
+       return preloadedGolfClubs
+   }
+}
